960bf9635f1b6085f1004384269dc308fe90a628
vitsi <vitsi@VITSIFUNK>
2024-11-25 17:22:04 +0200

    Add project files.

---
+++ Car/CarRental.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car
+{
+    public class CarRental
+    {
+        public CarRental(string firstName, string lastName, int numberOfDays)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            NumberOfDays = numberOfDays;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int NumberOfDays { get; set; }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Customer: {FirstName} {LastName}");
+            Console.WriteLine($"Rental Days: {NumberOfDays}");
+        }
+    
+    }
+}
---
+++ Car/Program.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarRental rental1 = new("Vaios", "Tsiaras", 10);
+            CarRental rental2 = new("Thanasis", "Thanasopoulos", 10);
+            Console.WriteLine("First Rent");
+            rental1.DisplayInfo();
+            rental2.DisplayInfo();
+
+            rental1.FirstName = "Giannis";
+            rental1.LastName = "Giannaros";
+            rental1.NumberOfDays = 1;
+
+            rental2.FirstName = "Eleni";
+            rental2.LastName = "Elenakou";
+            rental2.NumberOfDays = 12;
+
+            Console.WriteLine("\nUpdated Rental info: ");
+
+            rental1.DisplayInfo();
+            rental2.DisplayInfo();
+        }
+    }
+}
